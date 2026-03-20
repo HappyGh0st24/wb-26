8f6afd89c1845f39b017f190204a8a316ad8a7ad
HappyGh0st24 <pinchukdiana50@gmail.com>
2026-03-20 20:43:40 +0200

    Lab JS

---
+++ lab6/lab6/lab6.js
@@ -0,0 +1,39 @@
+//завдання 1//
+const a = 9;
+const b =3;
+console.log (a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+//Завдання 2//
+let a= "Привіт";
+let b ="Діана";
+console.log(`${a} ${b}`);
+//Завдання 3//
+let age=18;
+if (age>=18){
+    console.log("Доступ дозволено");
+} else{
+    console.log("Доступ заборонено")
+}
+//Завдання 4//
+let i= 1;
+i<= 10;
+for (let i=1; i<=10; i++);
+console.log(i);
+//Завдання 5//
+function square(number =5){
+    return (number*number);
+}
+console.log(square());
+//Завдання 6//
+let fruits=[];
+fruits.push("Мандарин","Яблоку","Банан");
+console.log(fruits);
+
+
+
+
+
+
+
